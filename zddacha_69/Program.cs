573906bb8639ba9e5b2049e92a9cf869b1332bb1
Larina <Losiknor@gmail.com>
2022-03-09 23:39:24 +0700

    Решение задачи 69

--- zddacha_69/Program.cs
+++ zddacha_69/Program.cs
@@ -1,4 +1,10 @@
 ﻿// Задача 69: Задайте значения M и N. Напишите программу, которая найдёт сумму элементов в промежутке от M до N.
 // M = 1; N = 4 -> 10
 // M = 4; N = 8. -> 30
-Console.WriteLine("Hello, World!");
+int SumNumber(int m, int n)
+{
+    if (n < m) return 0;
+    return SumNumber(m, n-1) + n;
+}
+// SumNumber(new Random().Next(20, 99), new Random().Next(1, 19));
+Console.Write(SumNumber(2,8));
