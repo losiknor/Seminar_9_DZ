ffac7bf13ade5784c425dbf3a2bdaa1f0cca71ef
Larina <Losiknor@gmail.com>
2022-03-09 23:05:14 +0700

    Решение задачи 67

--- zadacha_67/Program.cs
+++ zadacha_67/Program.cs
@@ -1,3 +1,10 @@
 ﻿// Задача 67: Показать натуральные числа от N до 1, N задано.
 // N = 5. -> "5, 4, 3, 2, 1"
-Console.WriteLine("Hello, World!");
+void ShowNumber(int n)
+{
+    if (n < 1) return;
+    
+    Console.Write(n + " ");
+    ShowNumber(n - 1);
+}
+ShowNumber(new Random().Next(10, 99));
